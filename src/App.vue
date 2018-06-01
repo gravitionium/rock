0467d5a5f0e83b6fde0e9342aea181e587b919c4
wupanhua <2547182011@qq.com>
2018-06-01 14:52:08 +0800

    稍微调整了一下代码格式

--- src/App.vue
+++ src/App.vue
@@ -5,35 +5,25 @@
           <el-row>
             <el-col :span="5" :offset="10">
               <div class="containerLogin">
-                <h1 class="loginTitle">I AM ROCk</h1>
+                <h1 class="loginTitle">I AM ROCK</h1>
                 <template>
                   <el-tabs v-model="activeName" @tab-click="handleClick">
                     <el-tab-pane label="邮箱登录" name="first">
                       <div class="loginMargin">
-                        <el-input
-                          placeholder="请输入内容"
-                          v-model="input10"
-                          clearable>
-                        </el-input>
+                        <el-input placeholder="请输入内容" v-model="input10" clearable></el-input>
                       </div>
                       <div class="loginMargin">
-                        <el-input
-                          placeholder="请输入内容"
-                          v-model="input11"
-                          clearable>
-                        </el-input>
+                        <el-input placeholder="请输入内容" v-model="input11" clearable> </el-input>
                       </div>
                       <el-row>
                         <el-button>登录</el-button>
                       </el-row>
-
                     </el-tab-pane>
                     <el-tab-pane label="微信" name="second">微信</el-tab-pane>
                     <el-tab-pane label="钉钉" name="third">钉钉</el-tab-pane>
                     <el-tab-pane label="企业微信" name="fourth">企业微信</el-tab-pane>
                   </el-tabs>
                 </template>
-                
               </div>
             </el-col>
           </el-row>
